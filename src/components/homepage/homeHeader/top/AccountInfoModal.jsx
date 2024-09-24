8a38bef7ab4ecf424d27f638a1b3e00f04f5fc1c
ndanaka <tanakanaoto632@gmail.com>
2024-09-24 13:18:47 -0400

    Add function to get payload from token

--- src/components/homepage/homeHeader/top/AccountInfoModal.jsx
+++ src/components/homepage/homeHeader/top/AccountInfoModal.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
+import { useAtom } from "jotai";
+import { tokenWithPersistenceAtom } from "../../../../atoms";
+
 import Account from "../../account/Index";
 
 const AccountInfoModal = ({ show, setShow }) => {
+  const [token, setToken] = useAtom(tokenWithPersistenceAtom);
+  const [payload, setPayload] = useState();
+
+  useEffect(() => {
+    setPayload(jwtDecode(token));
+  }, []);
+
   return (
     <>
       <div className="relative max-[600px]:mt-2 text-[15px]">
@@ -9,7 +22,7 @@ const AccountInfoModal = ({ show, setShow }) => {
           onClick={() => setShow(!show)}
           className="border-[1px] mb-2 border-emerald-600 bg-white font-semibold rounded-md px-5 py-1 drop-shadow-md shadow-emerald-600"
         >
-          {"Jhon"}&nbsp;&nbsp;&nbsp;
+          {payload?.fullName}&nbsp;&nbsp;&nbsp;
           <i className="fa fa-chevron-down text-[10px]" />
         </button>
         {show && <Account setShow={setShow} />}

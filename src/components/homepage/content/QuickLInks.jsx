031bf09b27ce8e8b8f458c8c1c913340ed519c58
ndanaka <tanakanaoto632@gmail.com>
2024-10-22 18:00:36 -0400

    fix url path

--- src/components/homepage/content/QuickLInks.jsx
+++ src/components/homepage/content/QuickLInks.jsx
@@ -7,21 +7,21 @@ const QuickLinks = () => {
     <>
       <div className="flex flex-col justify-center md:w-4/5 lg:w-3/5 max-[900px]:px-[5px] mx-auto mb-4">
         <div className="border-[1px] border-gray-400 ">
-          {/* <Links
+          <Links
             label={"Homepage"}
             icon={"fas fa-home"}
             link={`${ORIPA_BASE_URL}/user/index`}
-          /> */}
+          />
           <Links
             label={"Register"}
             icon={"fas fa-user-plus"}
             link={`${ORIPA_BASE_URL}/auth/register`}
           />
-          {/* <Links
+          <Links
             label={"Login"}
             icon={"fas fa-key"}
             link={`${ORIPA_BASE_URL}/auth/login`}
-          /> */}
+          />
         </div>
       </div>
     </>

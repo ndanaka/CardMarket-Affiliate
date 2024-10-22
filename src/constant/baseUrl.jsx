91ffd67785ce73f7ba0edec9fca368501ad5a7ca
ndanaka <tanakanaoto632@gmail.com>
2024-10-22 02:21:58 -0400

    Make https
    Make https

--- src/constant/baseUrl.jsx
+++ src/constant/baseUrl.jsx
@@ -1,12 +1,5 @@
 // Affiliate Base URL
-export const API_BASE_URL = "http://192.168.141.61:5000/api/affiliate";
-export const SERVER_URL = "http://192.168.141.61:5000";
+export const API_BASE_URL = "https://162.43.19.102:5000/api/affiliate";
+export const SERVER_URL = "https://162.43.19.102:5000";
 // Oripa Base URL
-export const ORIPA_BASE_URL = "http://192.168.141.61:3000";
-
-
-// // Affiliate Base URL
-// export const API_BASE_URL = "http://2.56.172.11:5000/api/affiliate";
-
-// // Oripa Base URL
-// export const ORIPA_BASE_URL = "http://2.56.172.11:3000";
+export const ORIPA_BASE_URL = "https://162.43.19.102:5000";
